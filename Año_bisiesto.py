b9429c684cfb1c2fa7420498aad171e696559c66
Haroldsvm <haroldmartinez628@gmail.com>
2023-05-28 12:07:16 -0500

    Validar si unh año es bisiesto o no

--- Año_bisiesto.py
+++ Año_bisiesto.py
@@ -5,4 +5,4 @@ else:
     if ano%4==0:
         print(ano," Es un año bisiesto,(tiene 366 días)")
     else:
-        print(ano," No es un año bisiesto (tiene 365 días)")nt("Es un número impar")visible en 5 y en 11")
+        print(ano," No es un año bisiesto (tiene 365 días)")
--- Triangulo_Números.py
+++
@@ -1,4 +0,0 @@
-for y in range(5):
- for x in range(0,y):
-   print(y,end=" ")
- print("")
